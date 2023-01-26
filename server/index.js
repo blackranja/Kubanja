13a1fa536a846e89eb31cd89aed856a5fded3dd7
Josphat karanja <josphatranja243@gmail.com>
2023-01-26 23:56:24 +0300

    Express Setup

--- server/index.js
+++ server/index.js
@@ -4,3 +4,25 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const express = require('express');
+
+app.use(bodyParser.json({
+    limit: "30mb",
+    extended: true
+}));
+app.use(bodyParser.urlencoded({
+    limit: "30mb",
+    extended: true
+}));
+app.use(cors());
+const CONNECTION_URL = "";
+
+const PORT = process.env.PORT || 3000;
+mongoose.connect(
+    CONNECTION_URL,
+    {
+        useNewurlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => app.listen(
+
+))
