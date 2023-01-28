34a4f76b99565d6594e2c1b5ee0eb0317a29b32d
Josphat karanja <josphatranja243@gmail.com>
2023-01-28 10:52:19 +0300

    ExpressJS Main Structure

--- server/index.js
+++ server/index.js
@@ -2,27 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
-const express = require('express');
-
+import postRoutes from './routes/post.js';
+const app = express();
 app.use(bodyParser.json({
-    limit: "30mb",
+    limit: '30mb',
     extended: true
-}));
+}))
 app.use(bodyParser.urlencoded({
-    limit: "30mb",
+    limit: '30mb',
     extended: true
-}));
-app.use(cors());
-const CONNECTION_URL = "";
-
-const PORT = process.env.PORT || 3000;
+}))
+app.use('/posts', postRoutes);
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
 mongoose.connect(
-    CONNECTION_URL,
-    {
-        useNewurlParser: true,
-        useUnifiedTopology: true
-    }
-).then(() => app.listen(
-
-))
+    CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => app.listen(PORT, () => console.log(`Server Runnig on port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+mongoose.set('useFindAndModify', false);
