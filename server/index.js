9c612089e741557057e996941b445877480d9313
Josphat karanja <josphatranja243@gmail.com>
2023-01-31 23:50:55 +0300

    Mongoose setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+//require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
+//import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -11,3 +15,15 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+console.log(process.env.MONGO_URI);
+console.log(process.env.PORT);
+/*
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
+*/
