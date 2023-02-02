f89ef0813a22194cc225d08bb70bb673b7777127
Josphat karanja <josphatranja243@gmail.com>
2023-02-02 15:49:46 +0300

    ExpressJs Framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Form from './components/Form/Form';
+import Post from './components/Posts/Post';
+
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+
+        </div>
     )
 }
 
